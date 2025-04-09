b2ef56a2febcde6ab13abb250eca47f888170d4a
banban2007 <heinhtoo421@gmail.com>
2025-04-09 15:05:16 +0630

    update

--- app/views/home_views.py
+++ app/views/home_views.py
@@ -7,6 +7,10 @@ def HomeSliderList(request, category_slug=None):
     ourservices = OurServiceModel.objects.all()
     ourservicelists = OurServiceListModel.objects.all()
     ourpartners = OurPartnerModel.objects.all()
+    counters = CounterModel.objects.all()
+    whychooseus = WhyChooseUsModel.objects.all()
+    ourteam = OurTeamMemberModel.objects.all()
+    customerfeedback = CustomerFeedbackModel.objects.all()
     
     categories = None
     projects = None
@@ -26,6 +30,10 @@ def HomeSliderList(request, category_slug=None):
         "ourpartner":ourpartners,
         "projects":projects,
         "project_count":project_count,
+        "counter":counters,
+        "whychooseus":whychooseus,
+        "ourteam":ourteam,
+        "customerfeedback":customerfeedback,
     }
     return render(request, 'index.html', context)
 
